a3df41ca375e9f50dce945764c25d583174d6733
Kaluã Bentes <kaluanbentes@gmail.com>
2025-04-05 15:54:43 -0300

    Feat: Create app

---
+++ apps/app/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "@blogtron/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
---
+++ apps/app/postcss.config.cjs
@@ -0,0 +1,2 @@
+const config = require("@blogtron/postcss-config")
+module.exports = config
